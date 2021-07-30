4198bb9a32e5323700c91440bcf860ec0e85e2ca
kikonavarro <78880702+kikonavarro@users.noreply.github.com>
2021-07-30 14:23:41 +0200

    corregimos enfrentamientos de cuartos y semis

--- roundOfFour.js
+++ roundOfFour.js
@@ -2,35 +2,52 @@ import { Round } from "./rounds.js";
 import { nextRound } from "./rounds.js";
 
 class NextRound extends Round {
-  constructor(name, teams = []) {
-    super(name);
-    this.teams = teams;
-  }
+	constructor(name, teams = []) {
+		super(name);
+		this.teams = teams;
+	}
 
-  setMatchs() {
-    let numberOfMatchs = this.teams.length / 2;
-    this.matchs = [];
-    for (let i = 0; i < numberOfMatchs; i++) {
-      const match = { home: "Home", away: "Away" };
-      this.matchs.push(match);
-    }
-  }
+	setMatchs() {
+		let numberOfMatchs = this.teams.length / 2;
+		this.matchs = [];
+		for (let i = 0; i < numberOfMatchs; i++) {
+			const match = { home: "Home", away: "Away" };
+			this.matchs.push(match);
+		}
+	}
 
-  selectTeams() {
-    this.homeTeams = [];
-    this.awayTeams = [];
-    for (let i = 0; i < this.teams.length; i++) {
-      if (i % 2 === 0) {
-        this.homeTeams.push(this.teams[i]);
-      } else {
-        this.awayTeams.push(this.teams[i]);
-      }
-    }
-    for (let i = 0; i < this.teams.length / 2; i++) {
-      this.matchs[i].home = this.homeTeams[i];
-      this.matchs[i].away = this.awayTeams[i];
-    }
-  }
+	selectTeams() {
+		this.homeTeams = [];
+		this.awayTeams = [];
+		for (let i = 0; i < this.teams.length; i++) {
+			if (i % 2 === 0) {
+				this.homeTeams.push(this.teams[i]);
+			} else {
+				this.awayTeams.push(this.teams[i]);
+			}
+		}
+		let j = this.teams.length / 2 - 1;
+		for (let i = 0; i < this.teams.length / 2; i++) {
+			this.matchs[i].home = this.homeTeams[i];
+			this.matchs[i].away = this.awayTeams[j];
+			j--;
+		}
+	}
+
+	selectTeamsForSemis() {
+		this.homeTeams = [];
+		this.awayTeams = [];
+		for (let i = 0; i < this.teams.length / 2; i++) {
+			this.homeTeams.push(this.teams[i]);
+		}
+		for (let i = this.teams.length / 2; i < this.teams.length; i++) {
+			this.awayTeams.push(this.teams[i]);
+		}
+		for (let i = 0; i < this.teams.length / 2; i++) {
+			this.matchs[i].home = this.homeTeams[i];
+			this.matchs[i].away = this.awayTeams[i];
+		}
+	}
 }
 
 console.log("==== CUARTOS DE FINAL ====");
@@ -39,7 +56,7 @@ console.log(roundOfFour.teams);
 roundOfFour.setMatchs();
 roundOfFour.selectTeams();
 roundOfFour.matchs.forEach((element) => {
-  roundOfFour.getWinner(element);
+	roundOfFour.getWinner(element);
 });
 
 export let semifinalClasificated = roundOfFour.winners;
@@ -47,10 +64,10 @@ export let semifinalClasificated = roundOfFour.winners;
 console.log("==== SEMIFINALES ====");
 const semiFinal = new NextRound("semiFinal", semifinalClasificated);
 semiFinal.setMatchs();
-semiFinal.selectTeams();
+semiFinal.selectTeamsForSemis();
 console.log(semiFinal.matchs);
 semiFinal.matchs.forEach((element) => {
-  semiFinal.getWinner(element);
+	semiFinal.getWinner(element);
 });
 
 export let finalClasificated = semiFinal.winners;
@@ -61,7 +78,7 @@ const consolation = new NextRound("consolacion", consolationClasificated);
 consolation.setMatchs();
 consolation.selectTeams();
 consolation.matchs.forEach((element) => {
-  consolation.getWinner(element);
+	consolation.getWinner(element);
 });
 
 console.log("==== FINAL ====");
@@ -69,5 +86,5 @@ const final = new NextRound("final", finalClasificated);
 final.setMatchs();
 final.selectTeams();
 final.matchs.forEach((element) => {
-  final.getWinner(element);
+	final.getWinner(element);
 });
